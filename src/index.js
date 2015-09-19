365a1a5e321876fe01358b3a52682b5d1f01f12b
Ari Porad <git@ariporad.com>
2015-09-19 08:19:34 -0700

    feat: add react-router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Router, Route } from 'react-router';
+
 import App from './App';
 
-React.render(<App />, document.getElementById('root'));
+React.render((
+  <Router>
+    <Route path="/" component={App} />
+  </Router>
+), document.getElementById('root'));
